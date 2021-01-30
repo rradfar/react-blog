bd8dc5957419325c6c8d27e002413f5bc77bac7f
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-01-30 15:46:17 -0800

    Add the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  return <p>Test</p>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <h1>Content to be added...</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
