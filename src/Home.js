6eeea5d6c7b8d385982f1954df50ab0af01154d8
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-01 15:48:57 -0800

    Add blog listing to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'John', id: 1 },
+    { title: 'Welcome!', body: 'lorem ipsum...', author: 'Patrick', id: 2 },
+    { title: 'Web dev tips', body: 'lorem ipsum...', author: 'John', id: 3 },
+  ]);
+
   return (
     <div className='home'>
-      <h2>Homepage</h2>
+      {blogs.map(blog => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
