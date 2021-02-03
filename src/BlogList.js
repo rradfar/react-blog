567407c9eab368a3ee4eab53e280f09e1dcc5d5d
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-03 13:06:20 -0800

    Handle deleting blog posts

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,12 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className='blog-list'>
+      <h2>{title}</h2>
       {blogs.map(blog => (
         <div className='blog-preview' key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,38 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'John', id: 1 },
-    { title: 'Welcome!', body: 'lorem ipsum...', author: 'Patrick', id: 2 },
-    { title: 'Web dev tips', body: 'lorem ipsum...', author: 'John', id: 3 },
+    {
+      title: 'My new website',
+      body: 'lorem ipsum...',
+      author: 'Michael',
+      id: 1,
+    },
+    {
+      title: 'Welcome!',
+      body: 'lorem ipsum...',
+      author: 'Patrick',
+      id: 2,
+    },
+    {
+      title: 'Web dev tips',
+      body: 'lorem ipsum...',
+      author: 'John',
+      id: 3,
+    },
   ]);
+
+  const handleDelete = id => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} />
+      <BlogList
+        blogs={blogs}
+        title='Featured Blogs'
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
