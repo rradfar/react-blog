2672c0662278a560ea283a37960a41f07df945d6
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-06 14:22:54 -0800

    Add a conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,20 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => res.json())
-      .then(data => setBlogs(data));
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      });
   }, []);
 
   return (
     <div className='home'>
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title='Featured Blogs' />}
     </div>
   );
