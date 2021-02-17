bfcfa90bbeb1b14157c1057917fcd6c1a7effe3f
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-17 11:36:41 -0800

    Create a form for new blog posts

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('John');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const blog = { title, body, author };
+  };
+
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={e => setBody(e.target.value)}
+        ></textarea>
+        <label>Author:</label>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
+          <option value='John'>John</option>
+          <option value='Michael'>Michael</option>
+          <option value='Patrick'>Patrick</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
