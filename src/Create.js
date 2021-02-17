62f7f3482fdca23f3930fcb781421c94cb567e56
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-17 11:50:05 -0800

    Make a Post request to send blog posts

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('John');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
     const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('New blog post added!');
+      setIsPending(false);
+    });
   };
 
   return (
@@ -33,7 +45,8 @@ const Create = () => {
           <option value='Michael'>Michael</option>
           <option value='Patrick'>Patrick</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
