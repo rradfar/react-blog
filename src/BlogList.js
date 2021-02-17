47b324a1a29f38520649950d4fa88a791c0ddf05
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-17 12:01:20 -0800

    Add the functionality for Delete button

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs }) => {
   return (
     <div className='blog-list'>
-      <h2>{title}</h2>
       {blogs.map(blog => (
         <div className='blog-preview' key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
